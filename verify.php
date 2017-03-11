8feead0ddd6769be23e0ee52ecc5c42aee9330dc
UIFS <thanaphoj.horthaisong@gmail.com>
2017-03-11 17:05:14 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,9 @@
 <?php
 $access_token = '0HSB7lPEM4zUCOYk9qQVNRJJndXzEeVW59UdLPzKN+NVPUAvbA9RCHCR3+pR57bHMZM1gsneTm5Dzd6iLH9fCJJlKRjD6T5Y0ierWK0e9vdgJ6tvqUMAA9tC6PsbsTYtzcpcYEHjTo+RWLMg0HrLHgdB04t89/1O/w1cDnyilFU=';
 
+$proxy = 'velodrome.usefixie.com:80';
+$proxyauth = 'fixie:gR62E1MpgXhcKpo';
+
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
@@ -9,6 +12,8 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
